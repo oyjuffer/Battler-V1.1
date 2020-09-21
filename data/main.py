24922748f859734bea690d94fc745a0447a917ea
oyjuffer <oyjuffer@outlook.com>
2020-09-21 17:15:55 +0300

    wrote gameloop

--- data/main.py
+++ data/main.py
@@ -2,9 +2,37 @@
 
 ### MAIN ###
 
-# IMPORT LIBRARIES 
+# IMPORT LIBRARIES
+import os 
 import pygame
 
 # IMPORT GAME FILES
-from constants import *
-from setup import *
+from . import constants
+from . import setup
+
+# CLASSES
+class Control():
+	def __init__(self):
+		self.running 	= True
+		self.clock 		= pygame.time.Clock()
+		self.fps		= 60
+
+	def eventloop(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				self.running = False
+
+	def mainloop(self):
+		while self.running:
+			self.eventloop() 
+			pygame.display.update()
+			self.clock.tick(self.fps)
+
+
+# FUNCTIONs 
+
+
+def main():
+	game = Control()
+
+	game.mainloop()
