f19e3cba8783e4b816b31c2bb269ada8a43b6955
oyjuffer <oyjuffer@outlook.com>
2020-09-21 17:16:07 +0300

    game setup

--- data/setup.py
+++ data/setup.py
@@ -6,13 +6,13 @@
 import pygame
 
 # IMPORT GAME FILES
-from constants import *
+from . import constants
 
 
 # INITIALIZATION
 pygame.init()
-pygame.display.set_caption("BATTLER V1.1")
-SCREEN = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption(constants.CAPTION)
+SCREEN = pygame.display.set_mode(constants.SCREEN_SIZE)
 
 
 print("setup.py RUN")
