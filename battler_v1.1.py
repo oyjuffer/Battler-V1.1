2f551798c07789cc32295fb39a12b11f0c48690e
oyjuffer <oyjuffer@outlook.com>
2020-09-21 17:14:35 +0300

    mainfile

---
+++ battler_v1.1.py
@@ -0,0 +1,19 @@
+
+
+### BATTLER V1.1 ###
+
+#IMPORT LIBRARIES 
+import sys
+import pygame
+
+# IMPORT GAME FILES
+from data.main import main
+
+
+
+if __name__ == "__main__":
+	main()
+	print("EXITING GAME")
+	pygame.quit()
+	sys.exit()
+
