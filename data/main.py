c00daef95e99ecda59780d5849b493592820215a
oyjuffer <oyjuffer@outlook.com>
2020-09-22 15:04:25 +0300

    set up state dict

--- data/main.py
+++ data/main.py
@@ -8,31 +8,21 @@ import pygame
 
 # IMPORT GAME FILES
 from . import constants
+from . import control
 from . import setup
+from .states import main_menu
 
 # CLASSES
-class Control():
-	def __init__(self):
-		self.running 	= True
-		self.clock 		= pygame.time.Clock()
-		self.fps		= 60
-
-	def eventloop(self):
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				self.running = False
-
-	def mainloop(self):
-		while self.running:
-			self.eventloop() 
-			pygame.display.update()
-			self.clock.tick(self.fps)
-
 
 # FUNCTIONs 
-
-
 def main():
-	game = Control()
-
+	"""
+	This is the main function for the game; it sets up a game object and state dictionaries.
+	It then sets up the initital active state and runs the mainloop. 
+	This can be found in control.py. 
+	"""
+	game = control.Control()
+	state_dict = {constants.MAIN_MENU: main_menu.Main_Menu()}
+	
+	game.setup_states(state_dict, constants.MAIN_MENU)
 	game.mainloop()
