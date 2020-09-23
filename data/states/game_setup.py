21915f4577b0d67153afd569f761717681b5e339
oyjuffer <oyjuffer@outlook.com>
2020-09-23 14:41:07 +0300

    added game setup file

---
+++ data/states/game_setup.py
@@ -0,0 +1,31 @@
+
+
+### GAME SETUP STATE ###
+
+# IMPORT LIBRARIES
+import os 
+import pygame
+
+# IMPORT GAME FILES
+from .. import constants
+from .. import control
+from .. import setup
+
+
+class Game_Setup():
+	def __init__(self):
+		self.running 		= False
+		self.state 			= None
+		self.menu_location	= 1
+
+	def setup(self):
+		pass
+
+	def events(self):
+		pass
+
+	def update(self):
+		pass
+
+	def draw(self):
+		pass
