e149977b7dbd38b4445ac4f1a8140da9731c3ba4
Yorick <oyjuffer@outlook.com>
2020-10-19 14:10:46 +0300

    removed arrow graphic

--- data/setup.py
+++ data/setup.py
@@ -16,8 +16,8 @@ pygame.display.set_caption(constants.CAPTION)
 SCREEN = pygame.display.set_mode(constants.SCREEN_SIZE)
 
 graphics_dir = os.path.join("resources","graphics")
-arrow = pygame.image.load(os.path.join(graphics_dir, "menu_arrow.png"))
-arrow_rect = arrow.get_rect()
+#arrow = pygame.image.load(os.path.join(graphics_dir, "menu_arrow.png"))
+#arrow_rect = arrow.get_rect()
 
 
 
