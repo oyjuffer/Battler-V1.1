2c2576e018546acd67c0d4e9bbde546fb2b3c075
oyjuffer <oyjuffer@outlook.com>
2020-10-21 13:03:50 +0300

    prep for reintroducing game setup state

--- data/states/game_setup.py
+++ data/states/game_setup.py
@@ -19,7 +19,7 @@ class Game_Setup():
 		self.menu_location	= 1
 
 	def setup(self):
-		pass
+		self.menu_location = 1
 
 	def events(self, keystate):
 		pass
@@ -28,4 +28,42 @@ class Game_Setup():
 		pass
 
 	def draw(self):
-		pass
+		control.draw_text("CHARACTER SETUP", 32, constants.BLACK, setup.SCREEN, "center", 250, 40)
+		control.draw_text("LEVEL 1", 24, constants.BLACK, setup.SCREEN, "center", 70, 75)
+		
+		# DRAWS SKILL POINTS
+		control.draw_text("SKILL",  24, constants.BLACK, setup.SCREEN, "center", 70, 125)
+		control.draw_text("POINTS",  24, constants.BLACK, setup.SCREEN, "center", 70, 145)
+		control.draw_text("3",  32, constants.BLACK, setup.SCREEN, "center", 125, 130)
+
+		# DRAWS STRENGTH 
+		control.draw_text("STR", 22, constants.BLACK, setup.SCREEN, "center", 185, 100)
+		pygame.draw.rect(setup.SCREEN, constants.BLACK, [160, 115, 50, 50], 5)
+
+		# DRAWS INTELLIGENCE
+		control.draw_text("INT", 22, constants.BLACK, setup.SCREEN, "center", 270, 100)
+		pygame.draw.rect(setup.SCREEN, constants.BLACK, [245, 115, 50, 50], 5)
+
+		# DRAW AGILITY
+		control.draw_text("AGI", 22, constants.BLACK, setup.SCREEN, "center", 355, 100)
+		pygame.draw.rect(setup.SCREEN, constants.BLACK, [330, 115, 50, 50], 5)
+
+
+		# DRAW CONSTITUTION
+		control.draw_text("CON", 22, constants.BLACK, setup.SCREEN, "center", 440, 100)
+		pygame.draw.rect(setup.SCREEN, constants.BLACK, [415, 115, 50, 50], 5)
+
+
+		# DRAW INVENTORY SLOTS
+		control.draw_text("Weapon", 26, constants.BLACK, setup.SCREEN, "center", 100, 205)
+		pygame.draw.rect(setup.SCREEN, constants.BLACK, [62.5, 222.5, 75, 75], 5)
+
+		control.draw_text("Armour", 26, constants.BLACK, setup.SCREEN, "center", 250, 205)
+		pygame.draw.rect(setup.SCREEN, constants.BLACK, [212.5, 222.5, 75, 75], 5)
+
+		control.draw_text("Token", 26, constants.BLACK, setup.SCREEN, "center", 400, 205)
+		pygame.draw.rect(setup.SCREEN, constants.BLACK, [362.5, 222.5, 75, 75], 5)
+
+
+		pygame.draw.rect(setup.SCREEN, constants.BLACK, [0, 175, 500, 10])
+		pygame.draw.rect(setup.SCREEN, constants.BLACK, [0, 350, 500, 10])
